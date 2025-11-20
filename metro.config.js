1670f692014b03884d1f2d862c6ab0cb95f0489d
Fandy <fandy@FandydeMac-mini.local>
2025-11-20 16:50:20 +0800

    beta

--- metro.config.js
+++ metro.config.js
@@ -2,7 +2,7 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
 
 /**
  * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
+ * https://reactnative.dev/docs/metro
  *
  * @type {import('metro-config').MetroConfig}
  */
@@ -14,6 +14,19 @@ const config = {
       buffer: require.resolve('@craftzdog/react-native-buffer'),
     },
   },
+}
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === 'crypto') {
+    // when importing crypto, resolve to react-native-quick-crypto
+    return context.resolveRequest(
+      context,
+      'react-native-quick-crypto',
+      platform,
+    )
+  }
+  // otherwise chain to the standard Metro resolver.
+  return context.resolveRequest(context, moduleName, platform)
 }
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config)
